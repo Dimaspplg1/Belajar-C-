686bc0e993e8b47160e016980a97f8e4315818a2
Muhammad Dimas Susanto <111961672+NiMpox@users.noreply.github.com>
2022-08-31 18:01:52 +0700

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std ;
+
+int main() {
+
+	cout << "Nama : Muhammad Dimas Susanto" << endl ;
+	cout << "Kelas : X PPLG 1" << endl ;
+	cout << "===============================" << endl ;
+	cout << "Angka1 =10" << endl ;
+	cout << "Angka2 =5" << endl ;
+	cout << "hasil:" << 10*5 << endl ;
+
+	return 0;
+
+}
